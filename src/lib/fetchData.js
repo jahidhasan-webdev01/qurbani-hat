daa1650c399693e11d91c8ca3b1440381fb354ad
jahidhasan-webdev01 <jahidhasan.webdev01@gmail.com>
2026-05-02 00:09:53 +0600

    remove url before in fetching data function

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -1,5 +1,6 @@
 export const getAnimals = async () => {
-    const res = await fetch(`${process.env.BETTER_AUTH_URL}/animals.json`);
+    // const res = await fetch(`${process.env.BETTER_AUTH_URL}/animals.json`);
+    const res = await fetch("/animals.json");
     const data = await res.json();
 
     return data;
